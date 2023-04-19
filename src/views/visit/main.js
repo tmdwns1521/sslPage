b024a0e28bd69764ad5fe8c874a0859b01ee95d8
OSJ <tmdwns1521@naver.com>
2023-04-19 17:26:27 +0900

    new_data

--- src/views/visit/main.js
+++ src/views/visit/main.js
@@ -1,9 +1,25 @@
 const ok1 = document.getElementById('ok1')
 
+function success({ coords, timestamp }) {
+	const latitude = coords.latitude;   // 위도
+	const longitude = coords.longitude; // 경도
+	
+	alert(`위도: ${latitude}, 경도: ${longitude}, 위치 반환 시간: ${timestamp}`);
+}
+
+function getUserLocation() {
+	if (!navigator.geolocation) {
+		throw "위치 정보가 지원되지 않습니다.";
+	}
+	navigator.geolocation.getCurrentPosition(success);
+}
+
+
 ok1.addEventListener("click", (e) => {
 	e.preventDefault;
 	const check1 = document.getElementById('check1')
 	check1.style.display = "none";
+	getUserLocation();
 	const check2 = document.getElementById('check2')
 	check2.style.display = "block";
 })
