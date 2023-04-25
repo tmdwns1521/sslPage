bfd8d6984ff3fbe95329dfe67e9151115b7a8b8c
OSJ <tmdwns1521@naver.com>
2023-04-25 10:27:58 +0900

    api

--- src/routers/visitor-router.js
+++ src/routers/visitor-router.js
@@ -14,11 +14,14 @@ visitorRouter.post('/visitor', async (req, res, next) => {
 	try {
 		const { team_name, employee_names, employee_nums, tels, reations, latitude, longitude, bank_name, bank_number, bank_admin } = req.body;
 
+		console.log(latitude);
+		console.log(longitude);
 		const address = await axios.get(`https://dapi.kakao.com/v2/local/geo/coord2address.json?input_coord=WGS84&x=${longitude}&y=${latitude}`, {
 			headers : {
 				"Authorization": "KakaoAK 838a3cef132ebc7e79bb9d570ab26d7b"
 			}
 		});
+		console.log(address.data);
 		const address_data = (address.data.documents[0].road_address.address_name);
 
 		res.status(201).json({ip, address_data, team_name, employee_names, employee_nums, tels, reations, bank_name, bank_number, bank_admin });
