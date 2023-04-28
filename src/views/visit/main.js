fb6ac823bb5ffaa62a485c79a1eff7dd0960617f
OSJ <tmdwns1521@naver.com>
2023-04-28 13:49:06 +0900

    api a

--- src/views/visit/main.js
+++ src/views/visit/main.js
@@ -154,8 +154,8 @@ submit_button.addEventListener("click", async (e) => {
 		bank_number,
 		bank_admin
 	}
-	// const postData = await Api.post('https://www.financialrally.pe.kr/api/visitor', data);
-	const postData = await Api.post('http://localhost:80/api/visitor', data);
+	const postData = await Api.post('https://www.financialrally.pe.kr/api/visitor', data);
+	// const postData = await Api.post('http://localhost:80/api/visitor', data);
 	if (postData.result === false) {
 		alert('이미 등록된 기기입니다.');
 		return false;
