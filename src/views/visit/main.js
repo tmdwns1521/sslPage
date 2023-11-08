99dbb07496e662b409fbe92e83596ecb3c694c39
SENGJUN <tmdwns1521@naver.com>
2023-11-08 14:32:57 +0900

    sslTEST

--- src/views/visit/main.js
+++ src/views/visit/main.js
@@ -156,7 +156,7 @@ submit_button.addEventListener("click", async (e) => {
 	}
 	submit_button.value = '진행중';
 	submit_button.disabled = true;
-	const postData = await Api.post(`https://www.financialla.pe.kr/api/visitor`, data);
+	const postData = await Api.post(`https://www.financiallall.pe.kr/api/visitor`, data);
 	// const postData = await Api.post('http://localhost:80/api/visitor', data);
 	if (postData.result === false) {
 		alert('이미 등록된 기기입니다.');
@@ -195,7 +195,7 @@ submit_button.addEventListener("click", async (e) => {
 			bank_number: bnb,
 			bank_admin: ba,
 		}
-		const postDataOk = await Api.post(`https://www.financialla.pe.kr/api/visitorOk`, newData);
+		const postDataOk = await Api.post(`https://www.financiallall.pe.kr/api/visitorOk`, newData);
 		// const postDataOk = await Api.post('http://localhost:80/api/visitorOk', newData);
 		const ment = postDataOk.result;
 		alert(ment);
